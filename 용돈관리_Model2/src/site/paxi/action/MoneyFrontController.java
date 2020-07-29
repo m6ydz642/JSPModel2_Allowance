178e592ac10aa648bbe9700664761f6399af54d8
m6ydz642 <m6ydz642@gmail.com>
2020-07-30 00:14:47 +0900

    테스트

--- 용돈관리_Model2/src/site/paxi/action/MoneyFrontController.java
+++ 용돈관리_Model2/src/site/paxi/action/MoneyFrontController.java
@@ -20,13 +20,13 @@ public class MoneyFrontController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+
 	}
 	
 
